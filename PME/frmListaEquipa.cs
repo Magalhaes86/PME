e591c885b75857fa2aa499906618f8c1bb6ebf80
Marcos Magalhães <desenvolvimentosamsys@outlook.pt>
2023-08-24 19:35:14 +0100

    Add project files.

---
+++ PME/frmListaEquipa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace PME
+{
+    public partial class frmListaEquipa : KryptonForm
+    {
+        public frmListaEquipa()
+        {
+            InitializeComponent();
+        }
+
+        private void frmListaEquipa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PME/frmVisita.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+
+namespace PME
+{
+    public partial class frmVisita : KryptonForm
+    {
+        public frmVisita()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVisita_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
